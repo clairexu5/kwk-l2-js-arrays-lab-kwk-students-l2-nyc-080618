5010bf8d2273d4903a948fbc707519bbb2f1861b
Claire Xu <claire02.xu@gmail.com>
2018-08-08 20:08:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-// Write your solutions here!
-const drivers = ['Milo', 'Otis', 'Garfield'];
+// Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name) {
   newArray = drivers.push(name);
   return newArray;
 }
 
 function destructivelyPrependDriver(name) {
-  newArray = drivers.pop(name);
-  return newArray;
+  thisArray = drivers.unshift(name);
+  return thisArray;
+}
+
+function destructivelyRemoveLastDriver() {
+  thisArray = drivers.pop();
+  return thisArray;
+}
+
+function destructivelyRemoveFirstDriver() {
+  thisArray = drivers.shift();
+  return thisArray;
+}
+
+function appendDriver(name){
+  var thatArray = ["Milo", "Otis", "Garfield"];
+  thatArray.push(name);
+  return thatArray;
+}
+
+function prependDriver(name){
+  var thatArray = ["Milo", "Otis", "Garfield"];
+  thatArray.unshift(name);
+  return thatArray;
+}
+
+function removeLastDriver(){
+  var thatArray = ["Milo", "Otis", "Garfield"];
+  thatArray.pop();
+  return thatArray;
+}
+
+function removeFirstDriver(){
+  var thatArray = ["Milo", "Otis", "Garfield"];
+  thatArray.shift();
+  return thatArray;
 }
