70d32f3c1266c37796bd1612c0cb1fdc1c8c8cd8
Claire Xu <claire02.xu@gmail.com>
2018-08-08 19:56:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your solution here!
+// Write your solutions here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendDriver(name) {
+  newArray = drivers.push(name);
+  return newArray;
+}
+
+function destructivelyPrependDriver(name) {
+  newArray = drivers.pop(name);
+  return newArray;
+}
